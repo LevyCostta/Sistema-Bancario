5fdf9217ba63a08129c021d5b0f1186b1dc7fd51
Levi Costa <levicostasouza@hotmail.com>
2025-01-07 14:19:47 -0300

    Saque
    Adicionado a função para sacar  saldo de uma conta

--- sistema/lib/areacliente.py
+++ sistema/lib/areacliente.py
@@ -21,11 +21,12 @@ Saldo: R${conta["Saldo"]}""")
             linha()
 
 def depositar(contas, nome_cliente):
+    numero_agencia = input('Digite o número da agência para depósito: ')
     numero_conta = int(input('Digite o número da conta para depósito: '))
-    valor_deposito = float(input('Digite o valor que deseja depositar: ').replace(",", "."))
+    valor_deposito = float(input('Digite o valor que deseja depositar: '))
     
     for conta in contas:
-        if conta['Titular'] == nome_cliente and conta['Número'] == numero_conta:
+        if conta['Titular'] == nome_cliente and conta['Número'] == numero_conta and conta['Agência'] == numero_agencia:
             if valor_deposito > 0:
                 conta['Saldo'] += valor_deposito
                 salvar_dadoscontas(contas)
@@ -34,18 +35,36 @@ def depositar(contas, nome_cliente):
             else:
                 print('O valor do depósito deve ser positivo.')
                 return
-    print('Conta não encontrada ou não pertence ao cliente.')
+    print('Conta ou agência não encontrada, ou não pertence ao cliente.')
+
+def sacar(contas, nome_cliente):
+    numero_agencia = input('Digite o número da agência para saque: ')
+    numero_conta = int(input('Digite o número da conta para saque: '))
+    valor_saque = float(input('Digite o valor que deseja sacar: '))
+    
+    for conta in contas:
+        if conta['Titular'] == nome_cliente and conta['Número'] == numero_conta and conta['Agência'] == numero_agencia:
+            if conta['Saldo'] >= valor_saque:
+                conta['Saldo'] -= valor_saque
+                salvar_dadoscontas(contas)
+                print(f'Saque de R${valor_saque} realizado com sucesso! Novo saldo: R${conta["Saldo"]}.')
+                return
+            else:
+                print('Saldo insuficiente para realizar o saque.')
+                return
+    print('Conta ou Agência não encontrada, ou não pertence ao cliente.')
 
 def menu_areacliente():
     contas = carregar_dadoscontas()
     nome_cliente = input('Digite o nome do cliente para iniciar: ').title()
     while True:
         cabecalho('ACESSO DO CLIENTE'.center(50))
-        resposta = menu(['Listar Minhas Contas Bancárias', 'Depositar', 'Sair'])
+        resposta = menu(['Listar Minhas Contas Bancárias', 'Depositar', 'Sacar', 'Sair'])
         if resposta == 1:
             listar_contas_cliente(contas, nome_cliente)
         elif resposta == 2:
             depositar(contas, nome_cliente)
         elif resposta == 3:
-            print("Saindo...")
+            sacar(contas, nome_cliente)
+        elif resposta ==4:
             break
